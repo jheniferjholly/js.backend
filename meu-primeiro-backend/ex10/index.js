0cce43f682ba87a63756c49606bccda84e71346d
Jhenifer Jholly de Abreu Teobaldo <jheniferjholly@gmail.com>
2026-08-03 12:01:10 -0300

    Add files via upload

---
+++ meu-primeiro-backend/ex10/index.js
@@ -0,0 +1,5 @@
+
+const modulomatematico= require('./calculadora');
+
+console.log("resultado da soma via modulo:",modulomatematico.somar(2,3));
+console.log("resultado da multiplicação",modulomatematico.multiplicar(4,5));
